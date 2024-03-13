308c04c4eddcaa8818cd52a446d2332749dff9ed
Akash  Rawat <85763275+barcode007@users.noreply.github.com>
2024-03-14 01:22:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+let fizzbuzz =functon (a){
+	for(let i=1;i<=100;i++){
+	if(a%3==0){
+		alert("fizz")
+	}
+	else if(a%5==0){
+		alert("buzz")
+	}
+	elseif(a%15==0){
+		alert("fizzbuzz")
+	}
+}
+}
